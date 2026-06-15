4beb612a703a086698bb22e54a63f88246474e22
Aiswarya R S <aiswaryaram025@gmail.com>
2026-06-15 17:59:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (34.86%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+        int a=1,b=2;
+        if(n<=2)
+        {
+            return n;
+        }
+        else
+        {
+            for(int i=3;i<=n;i++)
+            {
+                int c=a+b;
+                a=b;
+                b=c;
+            }
+        }
+        return b;
+    }
+}
